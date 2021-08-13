01544d9f314501f836fde89125be043f2b03ed67
supermads <madisonsfahey@gmail.com>
2021-08-13 16:37:52 -0700

    Stage 3 of Project

--- Convoy Shipping Company/task/convoy/convoy.py
+++ Convoy Shipping Company/task/convoy/convoy.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import sqlite3
 
 
 def convert_to_csv(file_name):
@@ -19,9 +20,7 @@ def convert_to_csv(file_name):
     return csv_file_name
 
 
-def correct_and_write_file():
-    file_name = input("Input file name\n")
-
+def correct_and_write_csv(file_name):
     if file_name[-3:] != "csv":
         file_name = convert_to_csv(file_name)
 
@@ -33,9 +32,11 @@ def correct_and_write_file():
         with open(file_name, "r") as unchecked_file:
             line_count = 0
             corrections = 0
+            column_names = []
 
             for line in unchecked_file:
                 line = line.replace("\n", "").split(",")
+                corrected_row = []
 
                 if line_count > 0:
                     for c in line:
@@ -46,11 +47,17 @@ def correct_and_write_file():
                                 correction_in_line = True
                             else:
                                 corrected_c += l
-
-                        file_writer.writerow([corrected_c])
+                        corrected_row.append(corrected_c)
 
                         if correction_in_line:
                             corrections += 1
+
+                    file_writer.writerow(corrected_row)
+
+                else:
+                    for c in line:
+                        column_names.append(c)
+                    file_writer.writerow(column_names)
 
                 line_count += 1
 
@@ -59,5 +66,61 @@ def correct_and_write_file():
         else:
             print(f"{corrections} cells were corrected in {checked_file_name}")
 
+    return checked_file_name, column_names
 
-correct_and_write_file()
+
+def create_database(file_name, column_names):
+    db_name = file_name[:-13] + ".s3db"
+
+    df = pd.read_csv(file_name)
+
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    create_command = """CREATE TABLE IF NOT EXISTS convoy ("""
+
+    for name in column_names:
+        if name == "vehicle_id":
+            create_command += f"{name} INT PRIMARY KEY, "
+        else:
+            create_command += f"{name} INT NOT NULL, "
+
+    create_command = create_command[0:-2] + ");"
+
+    cursor.execute(create_command)
+
+    entries_added = 0
+    for row in df.itertuples():
+        cursor.execute("""
+            INSERT INTO convoy 
+            VALUES (?,?,?,?)
+            """,
+            (row[1],
+            row[2],
+            row[3],
+            row[4])
+            )
+        entries_added += 1
+
+    conn.commit()
+    conn.close()
+
+    if entries_added == 1:
+        print(f"1 record was inserted into {db_name}")
+    else:
+        print(f"{entries_added} records were inserted into {db_name}")
+
+
+def main():
+    file_name = input("Input file name\n")
+
+    if file_name[-13:] != "[CHECKED].csv":
+        file_name, column_names = correct_and_write_csv(file_name)
+        create_database(file_name, column_names)
+    else:
+        with open(file_name, "r") as f:
+            column_names = f.readlines()[0].replace("\n", "").split(",")
+        create_database(file_name, column_names)
+
+
+main()
--- Convoy Shipping Company/task/tests.py
+++ Convoy Shipping Company/task/tests.py
@@ -3,19 +3,33 @@ from hstest.test_case import TestCase
 from os import path
 import shutil
 import re
+import sqlite3
 import os
 
 
 class EasyRiderStage1(StageTest):
     files_to_delete = []
-    files_to_check = ["data_one_xlsx.xlsx", "data_big_xlsx.xlsx", "data_one_csv.csv", "data_big_csv.csv"]
+    files_to_check = ["data_one_xlsx.xlsx", "data_big_xlsx.xlsx", "data_one_csv.csv", "data_big_csv.csv",
+                      "data_one_chk[CHECKED].csv", "data_big_chk[CHECKED].csv"]
+
+    @staticmethod
+    def remove_s3db_files(files):
+        for name in [names.split(".")[0].strip("[CHECKED]") + ".s3db" for names in files]:
+            name_del = os.path.join("test", name)
+            if path.exists(name_del):
+                os.remove(name_del)
 
     def generate(self) -> List[TestCase]:
+        self.remove_s3db_files(self.files_to_check)
         return [
-                TestCase(stdin=[self.prepare_file], attach=("data_one_xlsx.xlsx", 1, "line", 4, "cell", 488)),
-                TestCase(stdin=[self.prepare_file], attach=("data_big_xlsx.xlsx", 10, "line", 12, "cell", 5961)),
-                TestCase(stdin=[self.prepare_file], attach=("data_one_csv.csv", 1, None, 4, "cell", 488)),
-                TestCase(stdin=[self.prepare_file], attach=("data_big_csv.csv", 12, None, 12, "cell", 5961)),
+            TestCase(stdin=[self.prepare_file], attach=("data_one_xlsx.xlsx", 1, "line", 4, "cell", 488, "record")),
+            TestCase(stdin=[self.prepare_file], attach=("data_big_xlsx.xlsx", 10, "line", 12, "cell", 5961, "record")),
+            TestCase(stdin=[self.prepare_file], attach=("data_one_csv.csv", 1, None, 4, "cell", 488, "record")),
+            TestCase(stdin=[self.prepare_file], attach=("data_big_csv.csv", 10, None, 12, "cell", 5961, "record")),
+            TestCase(stdin=[self.prepare_file],
+                     attach=("data_one_chk[CHECKED].csv", 1, None, 4, "cell", 488, "record")),
+            TestCase(stdin=[self.prepare_file],
+                     attach=("data_big_chk[CHECKED].csv", 10, None, 12, "cell", 5961, "record")),
         ]
 
     def after_all_tests(self):
@@ -27,7 +41,7 @@ class EasyRiderStage1(StageTest):
 
     def prepare_file(self, output):
         file_name = self.files_to_check.pop(0)
-        shutil.copy(path.join("test", file_name), path.join("."))
+        shutil.copy(os.path.join("test", file_name), os.path.join("."))
         self.files_to_delete.append(file_name)
         return file_name
 
@@ -42,7 +56,8 @@ class EasyRiderStage1(StageTest):
         with open(file_name, 'r', encoding='utf-8') as file_csv:
             csv_len = len([x for x in file_csv]) - 1
             if csv_len != nr:
-                return f"Wrong number of lines in file {file_name}. Expected {nr}, found {csv_len}\ncheck if you have imported headers and all data is present;\ncheck if you have imported the appropriate sheet.)"
+                return f"Wrong number of lines in file {file_name}. Expected {nr}, found {csv_len}\n" + \
+                       "check if you have imported headers and all data is present;\ncheck if you have imported the appropriate sheet.)"
         return False
 
     @staticmethod
@@ -68,6 +83,65 @@ class EasyRiderStage1(StageTest):
             return f"Check data in {file_name}. Sum of integer should be {number}, found {count}"
         return False
 
+    @staticmethod
+    def checking_database(file_name, nr_lines, number):
+        conn = sqlite3.connect(file_name)
+        convoy = conn.cursor()
+
+        #  checking if table exists
+        try:
+            lines = convoy.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='convoy';").fetchall()
+        except sqlite3.DatabaseError as er:
+            return f"Attempting to read from the {file_name} database generates the error: {er}."
+        if lines[0][0] == 0:
+            return f"There is no table named 'convoy' in database {file_name}"
+
+        #  counting number of the records
+        lines = convoy.execute("SELECT COUNT(*) FROM convoy").fetchone()[0]
+        if lines != nr_lines:
+            return f"Wrong number of records in database {file_name}. Expected {nr_lines}, found {lines}"
+
+        #  checking column names
+        lines = convoy.execute('select * from convoy').description
+        if sorted([x[0] for x in lines]) != sorted(
+                ['vehicle_id', 'engine_capacity', 'fuel_consumption', 'maximum_load']):
+            return f"There is something wrong in {file_name}. Found column names: {[x[0] for x in lines]}. " + \
+                   "Expected four columns names: 'vehicle_id', 'engine_capacity', 'fuel_consumption', 'maximum_load'"
+
+        #  checking sum of cells
+        all_lines = convoy.execute("SELECT * FROM convoy")
+        try:
+            count = sum(sum(x) for x in all_lines.fetchall())
+        except TypeError:
+            return f"There is a value other than INTEGER in the table."
+        if count != number:
+            return f"Check data. Sum of integer in '.s3db' should be {number}, found {count}."
+
+        #  checking if PRIMARY KEY exists
+        all_lines = convoy.execute("SELECT * FROM convoy")
+        p_key = all_lines.fetchall()[0][0]
+        try:
+            convoy.execute(
+                f"INSERT INTO convoy(vehicle_id,engine_capacity,fuel_consumption,maximum_load) VALUES({p_key},0,0,0)")
+        except sqlite3.IntegrityError:
+            pass
+        else:
+            return f"There is no PRIMARY KEY parameter on column 'vehicle_id' in {file_name}."
+
+        #  checking if columns have an attribute NOT NULL
+        not_null = (('1000', 'Null', '1', '1'), ('1001', '1', 'Null', '1'), ('1002', '1', '1', 'Null'))
+        for values in not_null:
+            try:
+                convoy.execute(f"INSERT INTO convoy(vehicle_id,engine_capacity,fuel_consumption,maximum_load) "
+                               f"VALUES({values[0]},{values[1]},{values[2]},{values[3]})")
+            except sqlite3.IntegrityError:
+                pass
+            else:
+                return f"At least one of the columns has no 'NOT NULL' parameter in {file_name}."
+
+        conn.close()
+        return False
+
     def check(self, reply: str, result) -> CheckResult:
         if "input" not in reply.lower():
             return CheckResult.wrong(f"The first line of the output should be 'Input file name'")
@@ -77,8 +151,7 @@ class EasyRiderStage1(StageTest):
             return CheckResult.wrong(f"There is not enough lines in the output")
         file_name = result[0].split(".")
 
-#  => xlsx
-
+        #  => xlsx
         if file_name[1] == "xlsx":
 
             test = self.file_exist(f'{file_name[0]}.csv')
@@ -97,10 +170,9 @@ class EasyRiderStage1(StageTest):
             if len(reply) == 0:
                 return CheckResult.wrong(f"There is not enough lines in the output")
 
-
-#  => csv
-
-        if any([file_name[1] == "xlsx", all([file_name[1] == "csv", not ".".join(file_name).endswith("[CHECKED].csv")])]):
+        #  => csv
+        if any([file_name[1] == "xlsx",
+                all([file_name[1] == "csv", not ".".join(file_name).endswith("[CHECKED].csv")])]):
 
             test = self.file_exist(f'{file_name[0]}[CHECKED].csv')
             if test:
@@ -115,6 +187,26 @@ class EasyRiderStage1(StageTest):
                 return CheckResult.wrong(test)
 
             reply.pop(0)
+            if len(reply) == 0:
+                return CheckResult.wrong(f"There is not enough lines in the output")
+
+        #  => [CHECKED]csv
+        if any([file_name[1] == "xlsx", file_name[1] == "csv", ".".join(file_name).endswith("[CHECKED].csv")]):
+
+            test = self.file_exist(f'{file_name[0].strip("[CHECKED]")}.s3db')
+            if test:
+                return CheckResult.wrong(test)
+
+            test = self.checking_database(f'{file_name[0].strip("[CHECKED]")}.s3db', result[1], result[5])
+            if test:
+                return CheckResult.wrong(test)
+
+            test = self.check_output(result[1], result[6], reply[0], f'{file_name[0].strip("[CHECKED]")}.s3db')
+            if test:
+                return CheckResult.wrong(test)
+
+            reply.pop(0)
+
         return CheckResult.correct()
 
 
